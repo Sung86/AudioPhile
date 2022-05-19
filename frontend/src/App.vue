80eaad305360d4fed1efbb4ca361a026b61d66fa
sung <sung.m@tutamail.com>
2022-05-20 01:26:56 +1000

    initial frontend structure

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,16 @@
 <template>
+	<q-layout view="lHh Lpr lFf" style="height: 100vh">
+		<Header />
+		<q-page-container style="min-height: inherit">
+			<q-page>
+				<router-view />
+				<Footer />
+			</q-page>
+		</q-page-container>
+	</q-layout>
 </template>
 <script setup lang="ts">
-
+import { defineAsyncComponent } from 'vue';
+const Header = defineAsyncComponent(() => import('./modules/Header/index.vue'));
+const Footer = defineAsyncComponent(() => import('./modules/Footer/index.vue'));
 </script>
-<style></style>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,19 +3,40 @@ import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
-		// {
-		//   path: '/',
-		//   name: 'home',
-		//   component: HomeView
-		// },
-		// {
-		//   path: '/about',
-		//   name: 'about',
-		//   // route level code-splitting
-		//   // this generates a separate chunk (About.[hash].js) for this route
-		//   // which is lazy-loaded when the route is visited.
-		//   component: () => import('../views/AboutView.vue')
-		// }
+		{
+			path: '/',
+			name: 'Home',
+			component: () =>
+				import(/* webpackChunkName: "login" */ '@/modules/Home/index.vue'),
+		},
+		{
+			path: '/checkout',
+			name: 'Checkout',
+			component: () =>
+				import(
+					/* webpackChunkName: "checkout" */ '@/modules/Checkout/index.vue'
+				),
+		},
+		{
+			path: '/category',
+			name: 'Category',
+			component: () =>
+				import(
+					/* webpackChunkName: "category" */ '@/modules/Category/index.vue'
+				),
+		},
+		{
+			path: '/error404',
+			name: 'Error404',
+			component: () =>
+				import(
+					/* webpackChunkName: "error404" */ '@/modules/Error404/index.vue'
+				),
+		},
+		{
+			path: '/:catchAll(.*)',
+			redirect: '/error404',
+		},
 	],
 });
 
